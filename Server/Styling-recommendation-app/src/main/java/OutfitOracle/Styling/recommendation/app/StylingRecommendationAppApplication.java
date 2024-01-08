ca73db4c3ae95eaebc3d996b4c96c2aa6667a35f
Gwonyi Moon <102517739+M00NPANG@users.noreply.github.com>
2024-01-08 16:34:59 +0900

    create Server

---
+++ Server/Styling-recommendation-app/src/main/java/OutfitOracle/Styling/recommendation/app/StylingRecommendationAppApplication.java
@@ -0,0 +1,13 @@
+package OutfitOracle.Styling.recommendation.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StylingRecommendationAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StylingRecommendationAppApplication.class, args);
+	}
+
+}
---
+++ Server/Styling-recommendation-app/src/test/java/OutfitOracle/Styling/recommendation/app/StylingRecommendationAppApplicationTests.java
@@ -0,0 +1,13 @@
+package OutfitOracle.Styling.recommendation.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StylingRecommendationAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
