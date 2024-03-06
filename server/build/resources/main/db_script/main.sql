475f17e9c2a058570fb0414ca07b530eeedfc2b5
Gwonyi Moon <102517739+M00NPANG@users.noreply.github.com>
2024-03-07 00:40:43 +0900

    재수정. 회원가입 기능 추가 및 비밀번호 관련 램덤 난수 생성 기능

---
+++ server/build/resources/main/db_script/main.sql
@@ -0,0 +1,23 @@
+-- Database Name --
+
+CREATE DATABASE outfitoracle;
+
+USE outfitoracle;
+
+-- USER TABLE STRUCTURE --
+CREATE TABLE user (
+  user_id INT NOT NULL AUTO_INCREMENT,
+  password VARCHAR(255) NOT NULL,
+  email VARCHAR(255) NOT NULL UNIQUE,
+  name VARCHAR(100) NOT NULL,
+  PRIMARY KEY (user_id)
+);
+
+-- 이미지 테이블 생성 --
+CREATE TABLE image (
+    fileName VARCHAR(255) NOT NULL,
+    fileOriName VARCHAR(255) NOT NULL,
+    fileUrl VARCHAR(255) NOT NULL,
+    PRIMARY KEY (fileName)
+);
+
