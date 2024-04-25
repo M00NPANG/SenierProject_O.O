09b229ce40fffc1890d301a59f622c1c7023ca79
DESKTOP-SRIQIJ2\gwony <moonsky1124@naver.com>
2024-04-25 20:38:16 +0900

    post 저장 서비스 코드 변경 -> PostUpdateRequest객체의 내용을 PostDB의 형태에 맞게 수정 각각  이미지 URl을 기반으로 헤당 ClothesDB의 아이디를 찾아 저장

--- server/src/main/java/teamProject_Server/Service/PostService.java
+++ server/src/main/java/teamProject_Server/Service/PostService.java
@@ -2,28 +2,53 @@ package teamProject_Server.Service;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import teamProject_Server.DTO.PostUpdateRequest;
 import teamProject_Server.Domain.Post;
+import teamProject_Server.Repository.ClothesRepository;
 import teamProject_Server.Repository.PostRepository;
+import teamProject_Server.Repository.UserRepository;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 public class PostService {
-
     private final PostRepository postRepository;
+    private final ClothesRepository clothesRepository;
+    private final UserRepository userRepository;
 
     @Autowired
-    public PostService(PostRepository postRepository) {
+    public PostService(PostRepository postRepository, UserRepository userRepository, ClothesRepository clothesRepository) {
         this.postRepository = postRepository;
+        this.userRepository = userRepository;
+        this.clothesRepository = clothesRepository;
     }
 
-    // 게시물 저장
-    public Post savePost(Post post) {
-        return postRepository.save(post);
+    // post 저장
+    public void savePost(PostUpdateRequest request) {
+        Post post = new Post();
+        post.setUserEmail(request.getEmail());
+        post.setTop_id(clothesRepository. findIdByPhotoPath(request.getTopUrl()));
+        post.setBottom_id(clothesRepository.findIdByPhotoPath(request.getBottomUrl()));
+        post.setShoes_id(clothesRepository.findIdByPhotoPath(request.getShoesUrl()));
+        post.setAccessories_id(clothesRepository.findIdByPhotoPath(request.getAccessoriesUrl()));
+        post.setBag_id(clothesRepository.findIdByPhotoPath(request.getBagUrl()));
+
+        postRepository.save(post);
     }
 
-    // 해당 유저의 게시물 전송
+    // 유저 이메일이 저장된 포스트 클라이언트로 전송
     public List<Post> getPostsByUserEmail(String userEmail) {
+
         return postRepository.findByUserEmail(userEmail);
     }
+
+    // 모든 포스트를 조회하되, 주어진 userEmail을 제외하고 최대 20개까지만 반환
+    public List<Post> getPostsNotByUserEmail(String userEmail) {
+        return postRepository.findAll().stream()
+                .filter(post -> !post.getUserEmail().equals(userEmail))
+                .limit(20)
+                .collect(Collectors.toList());
+    }
+
 }
